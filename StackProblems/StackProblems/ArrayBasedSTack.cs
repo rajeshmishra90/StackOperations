b88e5a82e8c2abbca419b2dfab961840bb918554
MishraRajesh <rajeshmishra90@gmail.com>
2017-05-02 12:00:08 +0530

    Stack operations

---
+++ StackProblems/StackProblems/ArrayBasedSTack.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackProblems
+{
+    public class ArrayBasedStack
+    {
+        private static int[] arr = new int[100];
+        private static int top = -1;
+
+        public void Push(int el)
+        {
+            if (top != arr.Length - 1)
+            {
+                top = top + 1;
+                arr[top] = el;
+            }
+            else
+            {
+                throw new InvalidOperationException("Element can not be inserted");
+            }
+        }
+
+        public int Pop()
+        {
+            if (top != -1)
+            {
+                var temp = arr[top];
+                top = top - 1;
+                return temp;
+            }
+            else
+            {
+                throw new InvalidOperationException("No more element to remove");
+            }
+        }
+
+        public int Top()
+        {
+            if (top != -1)
+                return arr[top];
+            else
+                throw new InvalidOperationException("Stack is Empty");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Stack: ");
+            for (int i = 0; i <= top; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Program1
+    {
+        static void Main1(string[] args)
+        {
+            try
+            {
+                ArrayBasedStack stack = new ArrayBasedStack();
+                stack.Push(10);
+                stack.Print();
+                stack.Push(20);
+                stack.Print();
+                stack.Pop();
+                stack.Print();
+                stack.Push(30);
+                stack.Print();
+                stack.Push(40);
+                stack.Print();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ StackProblems/StackProblems/LinkedListBasedStack.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace StackProblems
+{
+    internal class Node
+    {
+        public int data { get; set; }
+        public Node Next { get; set; }
+    }
+
+    internal class LinkedList
+    {
+        public Node Head { get; set; } = null;
+
+        public void Insert(int data)
+        {
+            var node = new Node() { data = data, Next = null };
+            if (Head == null)
+                Head = node;
+            else
+            {
+                node.Next = Head;
+                Head = node;
+            }
+        }
+
+        public int Delete()
+        {
+            if (Head == null)
+                throw new InvalidOperationException("List is empty !!!");
+            var temp = Head.data;
+            Head = Head.Next;
+            return temp;
+        }
+
+        public void Print()
+        {
+            var list = Head;
+            Console.Write("Elements: ");
+            while (list != null)
+            {
+                Console.Write(list.data + " ");
+                list = list.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public class LinkedListBasedStack
+    {
+        LinkedList list = new LinkedList();
+        internal void Push(int v)
+        {
+            list.Insert(v);
+        }
+
+        internal void Print()
+        {
+            list.Print();
+        }
+
+        internal void Pop()
+        {
+            list.Delete();
+        }
+    }
+
+    internal class Program2
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                LinkedListBasedStack stack = new LinkedListBasedStack();
+                stack.Push(10);
+                stack.Print();
+                stack.Push(20);
+                stack.Print();
+                stack.Pop();
+                stack.Print();
+                stack.Push(30);
+                stack.Print();
+                stack.Push(40);
+                stack.Print();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
